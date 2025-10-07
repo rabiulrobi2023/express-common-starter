11a3e2d9b3c64aafb52c684ca940ccda755cb9f5
Rabiul Islam <rabiul.robi2023@gmail.com>
2025-10-07 19:26:40 +0600

    Some probem solve of starter pack

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,23 @@
 import { Application, Request, Response } from "express";
 import express from "express";
 import cors from "cors";
-import globalErrorHandler from "./middlewires/globalerrorhandler";
-import notFoundRoute from "./middlewires/notFoundRoute";
-import { router } from "./app/config/routes";
+
+import { envVariable } from "./app/config/envConfig";
+import { router } from "./app/routes";
+import globalErrorHandler from "./app/middlewires/globalerrorhandler";
+import notFoundRoute from "./app/middlewires/notFoundRoute";
 
 const app: Application = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/v1", router);
 app.get("/", (req: Request, res: Response) => {
-  res.send("Server is running...");
+  res.send({
+    message: "Server is running...",
+    environment: envVariable.NODE_ENV,
+    uptime: process.uptime().toFixed(2) + "s",
+    timeStamp: new Date().toDateString(),
+  });
 });
 app.use(globalErrorHandler);
 app.use(notFoundRoute);
